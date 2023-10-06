121f31f52695892d5d3855c8a4dae9ecb526fbb4
Esther <esther.ifes2021@gmail.com>
2023-10-06 10:57:12 -0300

    banco de dados em mySql e em postgreSql

--- Banco de dados/eventando_my.sql
+++ Banco de dados/eventando_my.sql
@@ -1,12 +1,12 @@
 -- DELETANDO AS TABELAS
 -- drop table USUARIO, EVENTO_PRESENCIAL, EVENTO_ONLINE, EVENTO, CONVITE, LISTA_CONVIDADOS, LOCALIZACAO, TIPO_LOGRADOURO, BAIRRO, CIDADE, ESTADO, TIPO_CONTATO, buffet, plataforma, Favorita, Possui_bairro_cidade, Possui_cidade_estado, Possui_tipo_contato_evento, Tem_tipo_contato_usuario;
 
--- CREATE DAS TABELAS
+-- CREATE TABLES
 CREATE TABLE USUARIO (
-    nome varchar (90),
+    nome varchar(90),
     data_nasc date,
     senha varchar(50),
-    id_usuario serial PRIMARY KEY,
+    id_usuario int AUTO_INCREMENT PRIMARY KEY,
     FK_ESTADO_id_estado int
 );
 
@@ -17,33 +17,32 @@ CREATE TABLE EVENTO_PRESENCIAL (
 );
 
 CREATE TABLE EVENTO_ONLINE (
-    link varchar (500),
+    link varchar(500),
     FK_plataforma_plataforma_PK int,
     FK_EVENTO_id_evento int PRIMARY KEY
 );
 
 CREATE TABLE EVENTO (
-    objetivo varchar (255),
-    data_prevista date,
-    atracoes varchar (300),
+    objetivo varchar(255),
+    data_prevista,
     privacidade_restrita BOOL,
     horario time,
     src_img text,
     nome varchar(100),
-    id_evento serial PRIMARY KEY,
+    id_evento int AUTO_INCREMENT PRIMARY KEY,
     FK_USUARIO_id_usuario int
 );
 
 CREATE TABLE CONVITE (
-    estilo varchar (20),
-    cores varchar (15),
-    id_convite serial PRIMARY KEY,
+    estilo varchar(20),
+    cores varchar(15),
+    id_convite int AUTO_INCREMENT PRIMARY KEY,
     FK_EVENTO_id_evento int
 );
 
 CREATE TABLE LISTA_CONVIDADOS (
-    nome_convidado varchar (90),
-    id_lista_convidados serial PRIMARY KEY,
+    nome_convidado varchar(90),
+    id_lista_convidados int AUTO_INCREMENT PRIMARY KEY,
     email_convidado varchar(150),
     FK_CONVITE_id_convite int
 );
@@ -52,42 +51,43 @@ CREATE TABLE LOCALIZACAO (
     numero int,
     logradouro varchar(250),
     cep varchar(9),
-    id_localizacao serial PRIMARY KEY,
+    id_localizacao int AUTO_INCREMENT PRIMARY KEY,
     FK_TIPO_LOGRADOURO_id_tipo_logradouro int,
     FK_BAIRRO_id_bairro int
 );
 
 CREATE TABLE TIPO_LOGRADOURO (
     descricao varchar(50),
-    id_tipo_logradouro serial PRIMARY KEY
+    id_tipo_logradouro int AUTO_INCREMENT PRIMARY KEY
 );
 
 CREATE TABLE CIDADE (
     descricao varchar(100),
-    id_cidade serial PRIMARY KEY
+    id_cidade int AUTO_INCREMENT PRIMARY KEY
 );
+
 CREATE TABLE BAIRRO (
     descricao varchar(100),
-    id_bairro serial PRIMARY KEY
+    id_bairro int AUTO_INCREMENT PRIMARY KEY
 );
 
 CREATE TABLE ESTADO (
     descricao varchar(2),
-    id_estado serial PRIMARY KEY
+    id_estado int AUTO_INCREMENT PRIMARY KEY
 );
 
 CREATE TABLE TIPO_CONTATO (
-    id_tipo_contato serial PRIMARY KEY,
+    id_tipo_contato int AUTO_INCREMENT PRIMARY KEY,
     contato varchar(50)
 );
 
 CREATE TABLE buffet (
-    buffet_PK serial NOT NULL PRIMARY KEY,
-    buffet varchar (100)
+    buffet_PK int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    buffet varchar(100)
 );
 
 CREATE TABLE plataforma (
-    plataforma_PK serial NOT NULL PRIMARY KEY,
+    plataforma_PK int AUTO_INCREMENT NOT NULL PRIMARY KEY,
     plataforma varchar(50)
 );
 
@@ -115,111 +115,101 @@ CREATE TABLE POSSUI_TIPO_CONTATO_EVENTO (
 CREATE TABLE TEM_TIPO_CONTATO_USUARIO (
     fk_USUARIO_id_usuario int,
     fk_TIPO_CONTATO_id_tipo_contato int,
-    descricao varchar (150)
+    descricao varchar(150)
 );
 
--- ALTER DAS TABELAS
+-- ALTER TABLES
 ALTER TABLE USUARIO ADD CONSTRAINT FK_USUARIO_2
     FOREIGN KEY (FK_ESTADO_id_estado)
     REFERENCES ESTADO (id_estado)
     ON DELETE RESTRICT;
- 
+
 ALTER TABLE EVENTO_PRESENCIAL ADD CONSTRAINT FK_EVENTO_PRESENCIAL_2
     FOREIGN KEY (FK_buffet_buffet_PK)
     REFERENCES buffet (buffet_PK)
     ON DELETE NO ACTION;
- 
+
 ALTER TABLE EVENTO_PRESENCIAL ADD CONSTRAINT FK_EVENTO_PRESENCIAL_3
     FOREIGN KEY (FK_EVENTO_id_evento)
     REFERENCES EVENTO (id_evento)
     ON DELETE CASCADE;
- 
+
 ALTER TABLE EVENTO_PRESENCIAL ADD CONSTRAINT FK_EVENTO_PRESENCIAL_4
     FOREIGN KEY (FK_LOCALIZACAO_id_localizacao)
     REFERENCES LOCALIZACAO (id_localizacao)
     ON DELETE RESTRICT;
- 
+
 ALTER TABLE EVENTO_ONLINE ADD CONSTRAINT FK_EVENTO_ONLINE_2
     FOREIGN KEY (FK_plataforma_plataforma_PK)
     REFERENCES plataforma (plataforma_PK)
     ON DELETE NO ACTION;
- 
+
 ALTER TABLE EVENTO_ONLINE ADD CONSTRAINT FK_EVENTO_ONLINE_3
     FOREIGN KEY (FK_EVENTO_id_evento)
     REFERENCES EVENTO (id_evento)
     ON DELETE CASCADE;
- 
+
 ALTER TABLE EVENTO ADD CONSTRAINT FK_EVENTO_2
     FOREIGN KEY (FK_USUARIO_id_usuario)
     REFERENCES USUARIO (id_usuario)
     ON DELETE CASCADE;
- 
+
 ALTER TABLE CONVITE ADD CONSTRAINT FK_CONVITE_2
     FOREIGN KEY (FK_EVENTO_id_evento)
     REFERENCES EVENTO (id_evento)
     ON DELETE CASCADE;
- 
+
 ALTER TABLE LISTA_CONVIDADOS ADD CONSTRAINT FK_LISTA_CONVIDADOS_2
     FOREIGN KEY (FK_CONVITE_id_convite)
     REFERENCES CONVITE (id_convite)
     ON DELETE CASCADE;
- 
+
 ALTER TABLE LOCALIZACAO ADD CONSTRAINT FK_LOCALIZACAO_2
     FOREIGN KEY (FK_TIPO_LOGRADOURO_id_tipo_logradouro)
     REFERENCES TIPO_LOGRADOURO (id_tipo_logradouro)
     ON DELETE CASCADE;
- 
+
 ALTER TABLE LOCALIZACAO ADD CONSTRAINT FK_LOCALIZACAO_3
     FOREIGN KEY (FK_BAIRRO_id_bairro)
     REFERENCES BAIRRO (id_bairro)
     ON DELETE CASCADE;
- 
+
 ALTER TABLE Favorita ADD CONSTRAINT FK_Favorita_1
     FOREIGN KEY (fk_EVENTO_id_evento)
     REFERENCES EVENTO (id_evento)
     ON DELETE SET NULL;
- 
+
 ALTER TABLE Favorita ADD CONSTRAINT FK_Favorita_2
     FOREIGN KEY (fk_USUARIO_id_usuario)
     REFERENCES USUARIO (id_usuario)
     ON DELETE SET NULL;
- 
-ALTER TABLE Possui_bairro_cidade ADD CONSTRAINT FK_Possui_1
+
+ALTER TABLE POSSUI_BAIRRO_CIDADE ADD CONSTRAINT FK_Possui_1
     FOREIGN KEY (fk_BAIRRO_id_bairro)
     REFERENCES BAIRRO (id_bairro)
     ON DELETE RESTRICT;
- 
-ALTER TABLE Possui_cidade_estado ADD CONSTRAINT FK_Possui_2
+
+ALTER TABLE POSSUI_CIDADE_ESTADO ADD CONSTRAINT FK_Possui_2
     FOREIGN KEY (fk_CIDADE_id_cidade)
     REFERENCES CIDADE (id_cidade)
     ON DELETE RESTRICT;
- 
-/*ALTER TABLE Possui_bairro_cidade ADD CONSTRAINT FK_Possui_3
-    FOREIGN KEY (fk_CIDADE_id_cidade)
-    REFERENCES CIDADE (id_cidade)
-    ON DELETE RESTRICT;*/
- 
-ALTER TABLE Possui_cidade_estado ADD CONSTRAINT FK_Possui_5
-    FOREIGN KEY (fk_ESTADO_id_estado)
-    REFERENCES ESTADO (id_estado)
-    ON DELETE RESTRICT;
- 
-ALTER TABLE Possui_tipo_contato_evento ADD CONSTRAINT FK_Possui_6
+
+ALTER TABLE POSSUI_TIPO_CONTATO_EVENTO ADD CONSTRAINT FK_Possui_6
     FOREIGN KEY (fk_TIPO_CONTATO_id_tipo_contato)
     REFERENCES TIPO_CONTATO (id_tipo_contato)
     ON DELETE SET NULL;
- 
-ALTER TABLE Possui_tipo_contato_evento ADD CONSTRAINT FK_Possui_4
+
+ALTER TABLE POSSUI_TIPO_CONTATO_EVENTO ADD CONSTRAINT FK_Possui_4
     FOREIGN KEY (fk_EVENTO_id_evento)
     REFERENCES EVENTO (id_evento)
     ON DELETE SET NULL;
- 
-ALTER TABLE Tem_tipo_contato_usuario ADD CONSTRAINT FK_Tem_1
+
+ALTER TABLE TEM_TIPO_CONTATO_USUARIO ADD CONSTRAINT FK_Tem_1
     FOREIGN KEY (fk_USUARIO_id_usuario)
     REFERENCES USUARIO (id_usuario)
     ON DELETE RESTRICT;
- 
-ALTER TABLE Tem_tipo_contato_usuario ADD CONSTRAINT FK_Tem_2
+
+ALTER TABLE TEM_TIPO_CONTATO_USUARIO ADD CONSTRAINT FK_Tem_2
     FOREIGN KEY (fk_TIPO_CONTATO_id_tipo_contato)
     REFERENCES TIPO_CONTATO (id_tipo_contato)
     ON DELETE SET NULL;
