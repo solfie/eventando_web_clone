afc14a27aa6b4e4d7694a3325e9a80cf5faf6262
20211tiimi0052 <esther.ifes2021@gmail.com>
2023-10-06 12:46:06 -0300

    conexões possiveis

--- BD/connect_mobile.php
+++ BD/connect_mobile.php
@@ -1,4 +1,3 @@
-<!-- CONEXÃO MOBILE -->
 <?php
     $host = 'localhost'; // Endereço do servidor PostgreSQL
     $port = '5432'; // Porta do servidor PostgreSQL (normalmente 5432)
@@ -14,23 +13,4 @@
     } catch (PDOException $e) {
         echo 'Erro na conexão: ' . $e->getMessage();
     }
-?>
-
-<!-- CONEXÃO WEB -->
-<?php
-    //Conexão com banco de dados
-    $servername = "localhost"; //endereço do servidor
-    $username="root";
-    $password="usbw";
-    $db_name="crud";
-
-    //pdo - somente orientado objeto
-    $connect = mysqli_connect($servername,$username,$password,$db_name);
-
-    //codifica com o caracteres ao manipular dados do banco de dados
-    //mysqli_set_charset($connect, "utf8");
-
-    if(mysqli_connect_error()):
-        echo "Falha na conexão: ". mysqli_connect_error();
-    endif;
 ?>
