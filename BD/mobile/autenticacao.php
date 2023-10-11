53a3d150334b238aaaebbaa4487cada343746009
esthermoraes <esther.ifes2021@gmail.com>
2023-10-11 18:56:24 -0300

    autenticação ok

--- BD/mobile/autenticacao.php
+++ BD/mobile/autenticacao.php
@@ -10,20 +10,20 @@
 	$senha = NULL;
 
 	// Método para extrair o login e senha via mod_php (Apache)
-	if (isset( $_SERVER['PHP_AUTH_USER'] ) ) {
+	if (isset( $_SERVER['PHP_AUTH_USER'])) {
 		$email = $_SERVER['PHP_AUTH_USER'];
 		$senha = $_SERVER['PHP_AUTH_PW'];
 	}
 	// Método para demais servers
 	elseif(isset( $_SERVER['HTTP_AUTHORIZATION'])) {
-		if(preg_match( '/^basic/i', $_SERVER['HTTP_AUTHORIZATION']))
+		if(preg_match('/^basic/i', $_SERVER['HTTP_AUTHORIZATION']))
 			list($email, $senha) = explode(':', base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6)));
 	}
 
 	// O método abaixo realiza o processo de autenticação. Ele retorna 
 	// true caso os parametros de login e senha estejam corretos
 	// false caso os parametros de login e senha estejam incorretos
-	function autenticar($db_con) {
+	function autenticar($db_con){
 		// Quando dentro de uma função, para acessar variáveis globais no php é
 		// necessário acessá-las via $GLOBALS.
 		$email = trim($GLOBALS['email']);
@@ -31,7 +31,7 @@
 		//$db_con = $GLOBALS['db_con'];
 		
 		// Verifica antes se o parâmetro de login foi enviado ao servidor
-		if(!is_null($email)) {
+		if(!is_null($email)){
 			// realiza a consula no bd pelo usuário login
 			$consulta = $db_con->prepare("SELECT senha FROM usuarios WHERE email='$email'");
 			$consulta->execute();
